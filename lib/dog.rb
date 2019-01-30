7656a9a15a84673b25d0b1f241cb602e20651872
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-30 19:36:09 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  def initialize(name, breed="Mutt")
+    @name=name
+    @breed=breed
+  end
+
+  def name
+    @name
+  end
+  def name=(dog_name)
+    @name=dog_name
+  end
+
+  def breed
+    @breed
+  end
+  def breed=(dog_breed)
+    @breed=dog_breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+class Person
+  def initialize(name)
+    @name=name
+  end
+  def name
+    @name
+  end
+  def name=(person_name)
+    @name=person_name
+  end
+end
