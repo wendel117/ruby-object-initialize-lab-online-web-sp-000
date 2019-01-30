831b9e19949875ae38c0e7aea6613841969e458d
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-30 19:25:59 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,11 @@
 class Person
-  def initialize(name, breed)
+  def initialize(name)
     @name=name
-    @breed=breed
   end
-
+  def name
+    @name
+  end
+  def name=(person_name)
+    @name=person_name
+  end
 end
